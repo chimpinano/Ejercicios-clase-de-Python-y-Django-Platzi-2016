12c95b9157bc11a5a87304ccb9278de54ce93bbb
Juan David Castro <juandavidcastrogallego@gmail.com>
2016-01-21 21:11:28 -0500

    Create archivos.py

---
+++ archivos.py
@@ -0,0 +1,15 @@
+import string
+import random
+
+
+fo = open("foo.txt", "wb")
+fo.write("Python es el mejor lenguaje.\nEs genial!!\n")
+for i in range(100):
+    texto = ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for i in range(10))
+    fo.write("%s" % texto)
+fo.close()
+
+fo2 = open("foo.txt", "r+")
+srti = fo2.read()
+print srti
+fo2.close()
